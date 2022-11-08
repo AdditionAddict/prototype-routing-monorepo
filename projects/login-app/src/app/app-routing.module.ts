899004d42ec79beff2623579f849dfd43d91f336
Andrew Allen <andrewallen1.6180@yahoo.co.uk>
2022-11-08 09:00:05 +0000

    added login app routing

--- projects/login-app/src/app/app-routing.module.ts
+++ projects/login-app/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "auth",
+    loadChildren: () => import("./pages/auth/auth.module").then(m => m.AuthModule),
+  },
+  {
+    path: "dashboard",
+    loadChildren: () => import("./pages/dashboard/dashboard.module").then(m => m.DashboardModule),
+  },
+  {
+    path: "pages/feature-one",
+    loadChildren: () => import("../../../feature-one/src/app/pages/feature/feature.module").then(m => m.FeatureModule),
+  },
+  { path: "", redirectTo: "auth", pathMatch: "full" },
+  { path: "**", redirectTo: "auth" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
